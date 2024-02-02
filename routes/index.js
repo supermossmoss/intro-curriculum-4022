c829903fda9048708cab4625c5e89a46af746701
ryota <ryota6114@gmail.com>
2024-02-02 10:22:30 +0900

    トップページの日付の形式を、YYYY/MM/DD HH:mm 形式に修正

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@
 const express = require('express');
 const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient({ log: [ 'query' ] });
+const prisma = new PrismaClient({ log: ['query'] });
+
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('Asia/Tokyo');
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
@@ -10,12 +17,17 @@ router.get('/', async (req, res, next) => {
   if (req.user) {
     const schedules = await prisma.schedule.findMany({
       where: { createdBy: parseInt(req.user.id) },
-      orderBy: { updatedAt: 'desc' }
+      orderBy: { updatedAt: 'desc' },
+    });
+    schedules.forEach((schedule) => {
+      schedule.formattedUpdatedAt = dayjs(schedule.updatedAt)
+        .tz()
+        .format('YYYY/MM/DD HH:mm');
     });
     res.render('index', {
       title: title,
       user: req.user,
-      schedules: schedules
+      schedules: schedules,
     });
   } else {
     res.render('index', { title: title, user: req.user });
